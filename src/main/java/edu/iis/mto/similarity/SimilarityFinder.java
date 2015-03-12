ef190f79e354d28d03fa8a33274758b7957307ec
Radek Adamus <adamus.r@gmail.com>
2015-03-12 14:13:53 +0100

    similarity finder implementation added

---
+++ src/main/java/edu/iis/mto/similarity/SimilarityFinder.java
@@ -0,0 +1,34 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SequenceSearch;
+
+public class SimilarityFinder {
+	private SequenceSearch searcher;
+
+	public SimilarityFinder(SequenceSearch searcher) {
+		this.searcher = searcher;
+	}
+
+	public double calculateJackardSimilarity(int[] seq1, int[] seq2) {
+		if (seq1.length == 0 && seq2.length == 0) {
+			return 1.0d;
+		}
+		if (seq1.length == 0 || seq2.length == 0) {
+			return 0.0d;
+		}
+		int intersectSize = calculateIntersect(seq1, seq2);
+		int unionSize = seq1.length + seq2.length - intersectSize;
+
+		return (double) intersectSize / (double) unionSize;
+	}
+
+	private int calculateIntersect(int[] seq1, int[] seq2) {
+		int intersectSize = 0;
+		for (int elem : seq1) {
+			if (searcher.search(elem, seq2).isFound()) {
+				intersectSize++;
+			}
+		}
+		return intersectSize;
+	}
+}
