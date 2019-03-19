7ccc190602ec1dbcffd8082c028cdbce3b074ab9
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-03-19 17:31:13 +0100

    code formating

--- src/main/java/edu/iis/mto/similarity/SimilarityFinder.java
+++ src/main/java/edu/iis/mto/similarity/SimilarityFinder.java
@@ -3,30 +3,32 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SimilarityFinder {
-	private SequenceSearcher searcher;
 
-	public SimilarityFinder(SequenceSearcher searcher) {
-		this.searcher = searcher;
-	}
+    private SequenceSearcher searcher;
 
-	public double calculateJackardSimilarity(int[] seq1, int[] seq2) {
-		if (seq1.length == 0 && seq2.length == 0) {
-			return 1.0d;
-		}
+    public SimilarityFinder(SequenceSearcher searcher) {
+        this.searcher = searcher;
+    }
 
-		int intersectSize = calculateIntersect(seq1, seq2);
-		int unionSize = seq1.length + seq2.length - intersectSize;
+    public double calculateJackardSimilarity(int[] seq1, int[] seq2) {
+        if (seq1.length == 0 && seq2.length == 0) {
+            return 1.0d;
+        }
 
-		return (double) intersectSize / (double) unionSize;
-	}
+        int intersectSize = calculateIntersect(seq1, seq2);
+        int unionSize = seq1.length + seq2.length - intersectSize;
 
-	private int calculateIntersect(int[] seq1, int[] seq2) {
-		int intersectSize = 0;
-		for (int elem : seq1) {
-			if (searcher.search(elem, seq2).isFound()) {
-				intersectSize++;
-			}
-		}
-		return intersectSize;
-	}
+        return (double) intersectSize / (double) unionSize;
+    }
+
+    private int calculateIntersect(int[] seq1, int[] seq2) {
+        int intersectSize = 0;
+        for (int elem : seq1) {
+            if (searcher.search(elem, seq2)
+                        .isFound()) {
+                intersectSize++;
+            }
+        }
+        return intersectSize;
+    }
 }
