dcc7697b5cdabbdac3b4ca2e7d76ed001b6d14ca
Zientak.David <Zenakz@github.com>
2020-03-31 15:02:46 +0200

    Added isSearchNotCalledForNullFirstSequence test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
@@ -6,33 +6,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderBehaviorTests {
+
     private int[] seq1 = null;
     private int[] seq2 = null;
     private SequenceSearcherMock sequenceSearcherMock;
     private SimilarityFinder similarityFinder;
 
-    @BeforeEach
-    public void prep() {
+    @BeforeEach public void prep() {
         sequenceSearcherMock = new SequenceSearcherMock();
         similarityFinder = new SimilarityFinder(sequenceSearcherMock);
     }
 
-    @Test
-    public void isSearchCalledForNonEmptySequence(){
-        seq1=new int[]{1};
-        seq2=new int[]{5};
+    @Test public void isSearchCalledForNonEmptySequence() {
+        seq1 = new int[] {1};
+        seq2 = new int[] {5};
         similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(1, sequenceSearcherMock.methodCallCounter);
     }
 
-    @Test
-    public void isSearchNotCalledForEmptySequence(){
-        seq1=new int[]{};
-        seq2=new int[]{5};
+    @Test public void isSearchNotCalledForEmptySequence() {
+        seq1 = new int[] {};
+        seq2 = new int[] {5};
         similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertEquals(0, sequenceSearcherMock.methodCallCounter);
     }
 
-
+    @Test public void isSearchNotCalledForNullFirstSequence() {
+        seq2 = new int[] {1, 2, 3};
+        assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(seq1, seq2));
+        assertEquals(0, sequenceSearcherMock.methodCallCounter);
+    }
 
 }
