b5c32f0226aba7017e4f1c56cb1c36634c41dbb5
Zientak.David <Zenakz@github.com>
2020-03-31 12:37:00 +0200

    Added tests for longer sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -51,4 +51,35 @@ class SimilarityFinderStateTest {
         double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
         assertEquals(DIFFERENT_SET, result);
     }
+
+    @Test void calculateDifferentBigSets(){
+        seq1 = new int[]{3,5,7,8,9,0};
+        seq2 = new int[]{71,42,54,6};
+        similarityFinder = new SimilarityFinder((e, seq) ->{
+            for(int num:seq){
+                if (num == e) {
+                    return SearchResult.builder().withFound(true).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        });
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        assertEquals(DIFFERENT_SET, result);
+    }
+
+    @Test void calculateSameBigSets(){
+        seq1 = new int[]{3,7,4,2,53,6,43,6,98,5,23,4};
+        similarityFinder = new SimilarityFinder((e, seq) ->{
+            for(int num:seq){
+                if (num == e) {
+                    return SearchResult.builder().withFound(true).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        });
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1,seq1);
+        assertEquals(SAME_SET, result);
+    }
 }
